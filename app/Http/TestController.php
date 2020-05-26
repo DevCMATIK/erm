ca66d0cba7ba4fc888b2532240a424ec2ae7d547
mrebolledo <maxi.rebolledo@gmail.com>
2020-05-26 10:02:46 -0400

    a

--- app/Http/TestController.php
+++ app/Http/TestController.php
@@ -92,7 +92,7 @@ class TestController extends SoapController
 
             $headerParams = array(
                 'codigoDeLaObra' => $codigoDeObra,
-                'timeStampOrigen' =>   Carbon::now()->toIso8601String()
+                'timeStampOrigen' =>   Carbon::now()->toIsoString()
             );
 
 
