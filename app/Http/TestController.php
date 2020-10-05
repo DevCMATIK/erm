85fc7f14fe74c5011e22634b272f7cd0836424ff
mrebolledo <maxi.rebolledo@gmail.com>
2020-10-05 09:53:28 -0300

    qwe

--- app/Http/TestController.php
+++ app/Http/TestController.php
@@ -4,6 +4,8 @@ namespace App\Http;
 
 use App\App\Controllers\Controller;
 use App\Domain\Client\CheckPoint\Indicator\CheckPointIndicator;
+use App\Domain\Client\Zone\Zone;
+use App\Domain\WaterManagement\Device\Sensor\Alarm\SensorAlarmLog;
 use App\Domain\WaterManagement\Device\Sensor\Chronometer\ChronometerTracking;
 use App\Domain\WaterManagement\Device\Sensor\Electric\ElectricityConsumption;
 use App\Domain\WaterManagement\Device\Sensor\Sensor;
@@ -19,30 +21,66 @@ class TestController extends Controller
     public function __invoke(Request $request)
     {
 
-     dd(
-         DB::connection('bioseguridad')->table('reports')
-             ->where('grd_id',1038)
-             ->first()->p2,
-         DB::connection('bioseguridad')->table('reports')
-         ->where('grd_id',1038)
-         ->first()
-     );
+        $zones = $this->getZones();
 
+        $alarm_logs =  SensorAlarmLog::join('sensor_alarms','sensor_alarms.id','=','sensor_alarm_logs.sensor_alarm_id')
+            ->join('sensors','sensors.id','=','sensor_alarms.sensor_id')
+            ->join('addresses','sensors.address_id','=','addresses.id')
+            ->join('devices','devices.id','=','sensors.device_id')
+            ->join('check_points','devices.check_point_id','=','check_points.id')
+            ->leftJoin('sub_zone_sub_elements','sub_zone_sub_elements.device_id','=','devices.id')
+            ->leftJoin('sub_zone_elements','sub_zone_sub_elements.sub_zone_element_id','=','sub_zone_elements.id')
+            ->leftJoin('sub_zones','sub_zones.id','=','sub_zone_elements.sub_zone_id')
+            ->leftJoin('users','sensor_alarm_logs.accused_by','=','users.id')
+            ->leftJoin('zones','zones.id','=','sub_zones.zone_id')
+            ->leftJoin('alarm_notifications','sensor_alarms.id','=','alarm_notifications.alarm_id')
+            ->whereIn('devices.id',$this->getDevicesId($zones))
+            ->whereNull('sensor_alarms.deleted_at')
+            ->whereNotNull('sensor_alarm_logs.end_date')
+            ->select([
+                'zones.name as zone',
+                'sub_zones.name as sub_zone',
+                'check_points.name as device',
+                'sensors.name as sensor',
+                'sensor_alarm_logs.start_date as start_date',
+                'sensor_alarm_logs.end_date as end_date',
+                'sensor_alarm_logs.last_value_readed as last_value',
+                'sensor_alarm_logs.first_value_readed as first_value_readed',
+                'sensor_alarm_logs.min_or_max as type',
+                'sensor_alarm_logs.accused as accused',
+                'sensor_alarm_logs.accused_at as accused_at',
+                'sensor_alarm_logs.id as log_id',
+                'users.first_name as first_name',
+                'users.last_name as last_name',
+            ])
+            ->take(50)
+            ->orderBy('sensor_alarm_logs.id','DESC')
+            ->get()->unique('log_id');
+
+        dd($alarm_logs);
     }
 
-    protected function getSensors($date)
+
+    protected function getZones()
     {
-        $first_date = Carbon::parse($date)->toDateString();
-        $second_date = Carbon::parse($date)->addDay()->toDateString();
-        return  Sensor::whereHas('type', $typeFilter = function ($q) {
-            return $q->where('slug','ee-e-activa')->orWhere('slug','ee-e-reactiva')->orWhere('slug','ee-e-aparente');
-        })->whereHas('analogous_reports', $reportsFilter = function($query) use ($first_date,$second_date){
-            return $query->whereRaw("analogous_reports.date between '{$first_date} 00:00:00' and '{$second_date} 00:01:00'");
-        })->with([
-            'type' => $typeFilter,
-            'device.check_point.sub_zones',
-            'analogous_reports' => $reportsFilter,
-            'consumptions'
-        ])->get();
+        return Zone::whereHas('sub_zones', $filter =  function($query){
+            $query->whereIn('id',Sentinel::getUser()->getSubZonesIds())->whereHas('configuration');
+        })->with( ['sub_zones' => $filter],'sub_zones.sub_elements')->get();
+    }
+
+    protected function getDevicesId($zones)
+    {
+        $ids = array();
+        foreach($zones as $zone) {
+            foreach($zone->sub_zones as $sub_zone) {
+                foreach($sub_zone->sub_elements as $sub_element) {
+                    if(Sentinel::getUser()->inSubZone($sub_zone->id)) {
+                        array_push($ids,$sub_element->device_id);
+                    }
+                }
+            }
+        }
+
+        return collect($ids)->unique()->toArray();
     }
 }
