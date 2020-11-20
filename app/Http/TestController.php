259240a47c68fd558638e2d457c7f9cd251c99e5
mrebolledo <maxi.rebolledo@gmail.com>
2020-11-20 09:52:39 -0300

    get sensors

--- app/Http/TestController.php
+++ app/Http/TestController.php
@@ -73,52 +73,19 @@ class TestController extends Controller
         dd($execution_time,ElectricityConsumption::count());
     }
 
-    protected function getAnalogousValue($trigger)
-    {
-        $sensor_address = $trigger->sensor->full_address;
-        $sensor_grd_id = $trigger->sensor->device->internal_id;
-        $disposition = $trigger->sensor->dispositions()->where('id',$trigger->sensor->default_disposition)->first();
-        if(!$disposition) {
-            $disposition = $trigger->sensor->dispositions()->first();
-        }
-        if($disposition) {
-            $valorReport = Report::where('grd_id',$sensor_grd_id)->first()->$sensor_address;
-            if($valorReport){
-                $ingMin = $disposition->sensor_min;
-                $ingMax = $disposition->sensor_max;
-                $escalaMin = $disposition->scale_min;
-                $escalaMax = $disposition->scale_max;
-                if($escalaMin == null && $escalaMax == null) {
-                    $data = ($ingMin * $valorReport) + $ingMax;
-                } else {
-                    $f1 = $ingMax - $ingMin;
-                    $f2 = $escalaMax - $escalaMin;
-                    $f3 = $valorReport - $escalaMin;
-                    if($f2 == 0) {
-                        $data = ((0)*($f3)) + $ingMin ;
-                    } else {
-                        $data = (($f1/$f2)*($f3)) + $ingMin ;
-                    }
-                }
-
-            }
-            if(isset($data)) {
-                return $data;
-            }
-        }
-
-        return false;
-
-
-    }
-
-    protected function getReceptorValue($trigger)
-    {
-        $sensor_address = $trigger->receptor->full_address;
-        $sensor_grd_id = $trigger->receptor->device->internal_id;
-
-        $report = Report::where('grd_id',$sensor_grd_id)->first();
-        return  $report->$sensor_address;
+    protected function getSensors($date){
+        $first_date = Carbon::parse($date)->toDateString();
+        $second_date = Carbon::parse($date)->addDay()->toDateString();
+        return  Sensor::whereHas('type', $typeFilter = function ($q) {
+            return $q->where('slug','ee-e-activa')->orWhere('slug','ee-e-reactiva')->orWhere('slug','ee-e-aparente');
+        })->whereHas('analogous_reports', $reportsFilter = function($query) use ($first_date,$second_date){
+            return $query->whereRaw("analogous_reports.date between '{$first_date} 00:00:00' and '{$second_date} 00:01:00'");
+        })->with([
+            'type' => $typeFilter,
+            'device.check_point.sub_zones',
+            'analogous_reports' => $reportsFilter,
+            'consumptions'
+        ])->get();
     }
 
 }
