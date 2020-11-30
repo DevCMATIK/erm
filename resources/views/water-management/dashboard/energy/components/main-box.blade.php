1b86c512b36dd324303e8552e494d1e5f6ed1845
mrebolledo <maxi.rebolledo@gmail.com>
2020-11-30 17:09:11 -0300

    number error

--- resources/views/water-management/dashboard/energy/components/main-box.blade.php
+++ resources/views/water-management/dashboard/energy/components/main-box.blade.php
@@ -1,8 +1,8 @@
-<div style="margin: 0 !important;" class="p-3 m-0 mb-1 {{ $bg ?? 'bg-primary-300' }} {{ (isset($extraClasses))?implode(' ',$extraClasses):'' }} rounded  text-white  @if($value > 99999999 && $value < 1000000000) pb-2 pt-4 @endif @if($value > 999999999) pb-2 pt-5 @endif">
+<div style="margin: 0 !important;" class="p-3 m-0 mb-1 {{ $bg ?? 'bg-primary-300' }} {{ (isset($extraClasses))?implode(' ',$extraClasses):'' }} rounded  text-white  @if(strlen($value) > 8 && strlen($value) < 10) pb-2 pt-4 @endif @if(strlen($value) >= 10) pb-2 pt-5 @endif">
     <div class="">
-        <h3 class="display-4 d-block l-h-n m-0 fw-500 main-box-number" @if($value > 99999999 && $value < 1000000000) style="font-size:2.5em;" @endif @if($value > 999999999) style="font-size:2.1em;" @endif>
+        <h3 class="display-4 d-block l-h-n m-0 fw-500 main-box-number" @if(strlen($value) > 8 && strlen($value) < 10) style="font-size:2.5em;" @endif @if(strlen($value) >= 10) style="font-size:2.1em;" @endif>
             <div class="main-box-value" style="margin: 0; float: left; margin-right:3px;">
-                {{ number_format($value,0,',','.') }}
+                @if($value < 0) 0 @else {{ number_format($value,0,',','.') }} @endif
             </div>
             <span class="fs-nano main-box-measure" >
                     {{ $measure }}
