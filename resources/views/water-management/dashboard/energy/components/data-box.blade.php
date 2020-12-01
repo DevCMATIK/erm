7e24415cf0ed99fb9f6745a161bf50030a9bad5c
mrebolledo <maxi.rebolledo@gmail.com>
2020-12-01 00:21:17 -0300

    a

--- resources/views/water-management/dashboard/energy/components/data-box.blade.php
+++ resources/views/water-management/dashboard/energy/components/data-box.blade.php
@@ -1,8 +1,8 @@
-<div class="p-2 {{ $bg ?? 'bg-primary-300' }} {{ (isset($extraClasses))?implode(' ',$extraClasses):'' }} rounded overflow-hidden position-relative text-white @if(isset($mb)) {{ $mb }} @else mb-g @endif @if(strlen($value) > 8 && strlen($value) < 10) pb-1 pt-2 @endif @if(strlen($value) >= 10) pb-1 pt-3 @endif">
+<div class="p-2 {{ $bg ?? 'bg-primary-300' }} {{ (isset($extraClasses))?implode(' ',$extraClasses):'' }} rounded overflow-hidden position-relative text-white @if(isset($mb)) {{ $mb }} @else mb-g @endif @if($value > 99999999 && $value < 1000000000) pb-1 pt-2 @endif @if($value > 999999999) pb-1 pt-3 @endif">
     <div class="">
-        <h3 class="display-4 d-block l-h-n m-0 fw-900" @if(strlen($value) > 8 && strlen($value) < 10) style="font-size:2.5em;" @endif @if(strlen($value) >= 10) style="font-size:2.1em;" @endif>
+        <h3 class="display-4 d-block l-h-n m-0 fw-900" @if($value > 99999999 && $value < 1000000000) style="font-size:2.5em;" @endif @if($value > 999999999) style="font-size:2.1em;" @endif>
             <div class="data-box-value" style="margin: 0; float: left; margin-right:5px;">
-                @if($value < 0) C/D @else {{ number_format($value,0,',','.') }} @endif
+                @if($value < 0) # @else {{ $value }} @endif
             </div>
             <span class="fs-nano">
                     {{ $measure }}
