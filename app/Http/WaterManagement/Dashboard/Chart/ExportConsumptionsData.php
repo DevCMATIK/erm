576e653cc15418dc909d300828ef417d9f73d74d
mrebolledo <maxi.rebolledo@gmail.com>
2020-12-03 12:10:59 -0300

    ok

--- app/Http/WaterManagement/Dashboard/Chart/ExportConsumptionsData.php
+++ app/Http/WaterManagement/Dashboard/Chart/ExportConsumptionsData.php
@@ -14,12 +14,12 @@ class ExportConsumptionsData extends Controller
     public function __invoke(Request $request,$sub_zone_id)
     {
        $sub_zone = SubZone::with('zone')->find($sub_zone_id);
-       dd($sub_zone);
+
         $fileName = $sub_zone->zone->name.'-'.$sub_zone->name.'-Consumos.xlsx';
         if(strlen($fileName) > 31) {
             $fileName = substr($sub_zone->zone->name.'-'.$sub_zone->name,'0','20').'-Consumos.xlsx';
         }
-        return (new ExportConsumptions($request,$sub_zone))
+        return (new ExportConsumptions($sub_zone,$request))
             ->download($fileName);
     }
 }
