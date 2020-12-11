6f30db38637b17d59ce8f9301aee0625e0c41afe
mrebolledo <maxi.rebolledo@gmail.com>
2020-12-11 02:19:08 -0300

    a

--- app/Http/TestController.php
+++ app/Http/TestController.php
@@ -3,6 +3,7 @@
 namespace App\Http;
 
 use App\App\Controllers\Controller;
+use App\App\Controllers\Soap\InstanceSoapClient;
 use App\App\Traits\ERM\HasAnalogousData;
 use App\Domain\Client\CheckPoint\CheckPoint;
 use App\Domain\WaterManagement\Device\Device;
@@ -19,32 +20,79 @@ class TestController extends Controller
         $time_start = microtime(true);
 
         $checkPoints = $this->getCheckPoints(1);
-        $sensors = array();
-        $tote = array();
-        $flow = array();
-        $level = array();
         foreach($checkPoints as $checkPoint)
         {
             if(!isset($checkPoint->last_report) || $this->calculateTimeSinceLastReport($checkPoint) > 40) {
                 $sensors = $this->getSensors($checkPoint);
                 if(count($sensors) == 3) {
-                    $tote[] = $this->getAnalogousValue($sensors->where('name','Aporte')->first(),true);
-                    $flow[] = $this->getAnalogousValue($sensors->where('name','Caudal')->first(),true);
-                    $level[] = $this->getAnalogousValue($sensors->where('name','Nivel')->first(),true);
+                    $this->ReportToDGA(
+                        $this->getAnalogousValue($sensors->where('name','Aporte')->first(),true),
+                        $this->getAnalogousValue($sensors->where('name','Caudal')->first(),true),
+                        ($this->getAnalogousValue($sensors->where('name','Nivel')->first(),true) * -1),
+                        $checkPoint->work_code,
+                        $checkPoint
+                    );
                 } else {
                     continue;
                 }
             }
         }
 
-
-
         $time_end = microtime(true);
 
         $execution_time = ($time_end - $time_start);
 
-        dd($execution_time,$tote,$flow,$level);
+        dd($execution_time);
 
+    }
+
+    public function ReportToDGA($totalizador,$caudal,$nivelFreatico,$codigoDeObra,$checkPoint)
+    {
+        try {
+            ini_set('default_socket_timeout', 600);
+            self::setWsdl('https://snia.mop.gob.cl/controlextraccion/wsdl/datosExtraccion/SendDataExtraccionService');
+            $service = InstanceSoapClient::init();
+            $params = [
+                'sendDataExtraccionRequest' => [
+                    'dataExtraccionSubterranea' => [
+                        'fechaMedicion' => Carbon::now()->format('d-m-Y'),
+                        'horaMedicion' => Carbon::now()->format('H:i:s'),
+                        'totalizador' => number_format($totalizador,0,'',''),
+                        'caudal' => number_format($caudal,2,'.',''),
+                        'nivelFreaticoDelPozo' => number_format($nivelFreatico,2,'.',''),
+                    ]
+                ]
+            ];
+
+            $headerParams = array(
+                'codigoDeLaObra' => $codigoDeObra,
+                'timeStampOrigen' =>   Carbon::now()->toIso8601ZuluString()
+            );
+
+
+            $headers[] = new SoapHeader('http://www.mop.cl/controlextraccion/xsd/datosExtraccion/SendDataExtraccionRequest',
+                'sendDataExtraccionTraza',
+                $headerParams
+            );
+
+            $service->__setSoapHeaders($headers);
+
+            $response = $service->__soapCall('sendDataExtraccionOp',$params);
+
+            $checkPoint->dga_reports()->create([
+                'response' => $response->status->Code,
+                'response_text' => $response->status->Description,
+                'tote_reported' => $totalizador,
+                'flow_reported' => $caudal,
+                'water_table_reported' => $nivelFreatico,
+                'report_date' => Carbon::now()->toDateTimeString()
+            ]);
+
+
+
+        } catch(\Exception $e) {
+            return $e->getMessage();
+        }
     }
 
     protected function getCheckPoints($dga_report)
