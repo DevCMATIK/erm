37dd4e8b83157b17c4343b19e7c1a057b88b22bc
mrebolledo <maxi.rebolledo@gmail.com>
2020-12-20 17:42:01 -0300

    chks

--- app/Http/TestController.php
+++ app/Http/TestController.php
@@ -22,78 +22,22 @@ class TestController extends SoapController
         $time_start = microtime(true);
 
         $checkPoints = $this->getCheckPoints(1);
+        $chks = array();
         foreach($checkPoints as $checkPoint)
         {
-                $sensors = $this->getSensors($checkPoint);
-                if(count($sensors) == 3) {
-                    $this->ReportToDGA(
-                        $this->getAnalogousValue($sensors->where('name','Aporte')->first(),true),
-                        $this->getAnalogousValue($sensors->where('name','Caudal')->first(),true),
-                        ($this->getAnalogousValue($sensors->where('name','Nivel')->first(),true) * -1),
-                        $checkPoint->work_code,
-                        $checkPoint
-                    );
-                } else {
-                    continue;
-                }
+            $chks[] = $this->getSensors($checkPoint)->toArray();
+
         }
 
         $time_end = microtime(true);
 
         $execution_time = ($time_end - $time_start);
 
-        dd($execution_time);
+        dd($execution_time,$chks);
 
     }
 
-    public function ReportToDGA($totalizador,$caudal,$nivelFreatico,$codigoDeObra,$checkPoint)
-    {
-        try {
-            ini_set('default_socket_timeout', 600);
-            self::setWsdl('https://snia.mop.gob.cl/controlextraccion/wsdl/datosExtraccion/SendDataExtraccionService');
-            $service = InstanceSoapClient::init();
-            $params = [
-                'sendDataExtraccionRequest' => [
-                    'dataExtraccionSubterranea' => [
-                        'fechaMedicion' => Carbon::now()->format('d-m-Y'),
-                        'horaMedicion' => Carbon::now()->format('H:i:s'),
-                        'totalizador' => number_format($totalizador,0,'',''),
-                        'caudal' => number_format($caudal,2,'.',''),
-                        'nivelFreaticoDelPozo' => number_format($nivelFreatico,2,'.',''),
-                    ]
-                ]
-            ];
 
-            $headerParams = array(
-                'codigoDeLaObra' => $codigoDeObra,
-                'timeStampOrigen' =>   Carbon::now()->toIso8601ZuluString()
-            );
-
-
-            $headers[] = new SoapHeader('http://www.mop.cl/controlextraccion/xsd/datosExtraccion/SendDataExtraccionRequest',
-                'sendDataExtraccionTraza',
-                $headerParams
-            );
-
-            $service->__setSoapHeaders($headers);
-
-            $response = $service->__soapCall('sendDataExtraccionOp',$params);
-
-            $checkPoint->dga_reports()->create([
-                'response' => $response->status->Code,
-                'response_text' => $response->status->Description,
-                'tote_reported' => $totalizador,
-                'flow_reported' => $caudal,
-                'water_table_reported' => $nivelFreatico,
-                'report_date' => Carbon::now()->toDateTimeString()
-            ]);
-
-
-
-        } catch(\Exception $e) {
-            return $e->getMessage();
-        }
-    }
 
     protected function getCheckPoints($dga_report)
     {
@@ -111,8 +55,20 @@ class TestController extends SoapController
     protected function getSensors($checkPoint)
     {
         return $this->getSensorsByCheckPoint($checkPoint->id)
-            ->whereIn('name',['Nivel','Aporte','Caudal'])
-            ->get();
+            ->whereIn('type_id',function($query){
+                $query->select('id')->from('sensor_types')
+                ->whereIn('slug',[
+                    'tx-nivel',
+                    'caudal-dga-arkon-modbus',
+                    'caudal-dga-siemens-modbus',
+                    'caudal-dga-wellford-corriente',
+                    'caudal-dga-wellford-modbus',
+                    'totalizador-dga-arkon-modbus',
+                    'totalizador-dga-siemens-modbus',
+                    'totalizador-dga-wellford-modbus',
+                    'totalizador-dga-wellford-pulsos'
+                ]);
+            })->get();
     }
 
 
