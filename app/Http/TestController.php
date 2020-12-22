326efcce305422ef684bfa2644dae0fc3b17b541
mrebolledo <maxi.rebolledo@gmail.com>
2020-12-22 10:48:44 -0300

    Update TestController.php

--- app/Http/TestController.php
+++ app/Http/TestController.php
@@ -35,7 +35,6 @@ class TestController extends SoapController
                 $flow = $this->getFlowSensor($sensors);
                 $level = $this->getLevelSensor($sensors);
                 if($tote && $flow && $level) {
-                    if(optional($tote->dispositions)->first() && optional($flow->dispositions)->first() && optional($level->dipositions)->first) {
 
                         $chks[] = [
                             $tote->toArray(),
@@ -47,7 +46,7 @@ class TestController extends SoapController
                             $checkPoint->work_code,
                             $checkPoint
                         ];
-                    }
+                    
                 } else {
                     continue;
                 }
