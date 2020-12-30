1bb50b8850797b02107e0a5e60d0349080d33486
mrebolledo <maxi.rebolledo@gmail.com>
2020-12-30 18:30:05 -0300

    Update TestController.php

--- app/Http/TestController.php
+++ app/Http/TestController.php
@@ -27,7 +27,7 @@ class TestController extends SoapController
         $first_date = now()->subDay()->toDateString();
         $second_date = now()->toDateString();
 
-        dd(WaterConsumption::get());
+        dd(WaterConsumption::get()->toJson());
 
         $time_end = microtime(true);
 
