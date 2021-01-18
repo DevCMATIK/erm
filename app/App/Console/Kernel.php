dc6e1bd34d550a13d0964c1c3a9d96286d3844fe
mrebolledo <maxi.rebolledo@gmail.com>
2021-01-18 11:32:07 -0300

    dcs

--- app/App/Console/Kernel.php
+++ app/App/Console/Kernel.php
@@ -15,6 +15,7 @@ use App\Http\Data\Jobs\CheckPoint\ReportToDGA;
 use App\Http\Data\Jobs\Device\NotifyDevicesOffline;
 use App\Http\Data\Jobs\Device\TrackDeviceDisconnections;
 use App\Http\Data\Jobs\Device\TrackDisconnections;
+use App\Http\Data\Jobs\Device\TrackDisconnectionsFromBio;
 use App\Http\Data\Jobs\ProcessData;
 use App\Http\Data\Jobs\Sensors\BackupAnalogousSensors;
 use App\Http\Data\Jobs\Sensors\BackupDigitalSensors;
@@ -79,6 +80,7 @@ class Kernel extends ConsoleKernel
         $schedule->job(new SendReminderMail())->everyThirtyMinutes();
         $schedule->job(new NotifyDevicesOffline())->hourly();
         $schedule->job(new TrackDisconnections(),'tracking-queue')->everyMinute();
+        $schedule->job(new TrackDisconnectionsFromBio(),'tracking-queue')->everyMinute();
 
         // Get all tasks from the database
         $mailReports = MailReport::active()->get();
---
+++ app/Http/Data/Jobs/Device/TrackDisconnectionsFromBio.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Data\Jobs\Device;
+
+use App\Domain\WaterManagement\Device\Device;
+use Carbon\Carbon;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class TrackDisconnectionsFromBio implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $devices =  Device::with('report','last_dc')->where('from_bio',1)->get()->filter(function($device){
+            $state =  DB::connection('bioseguridad')->table('reports')
+                ->where('grd_id',optional($device)->internal_id)
+                ->first()->state;
+
+            return  $state  === 0 ||
+                (
+                    optional($device->last_disconnection->first())->start_date != ''
+                    &&
+                    optional($device->last_disconnection->first())->end_date == null
+                );
+        });
+
+        foreach($devices as  $device){
+
+            $state =  DB::connection('bioseguridad')->table('reports')
+                ->where('grd_id',optional($device)->internal_id)
+                ->first()->state;
+
+            if(optional($device->last_dc)->start_date != '' && optional($device->last_dc)->end_date == null) {
+                if($state === 0) {
+                    continue;
+                } else {
+                    $last = $device->last_dc;
+                    $last->end_date = Carbon::now()->toDateTimeString();
+                    $last->save();
+                }
+            } else {
+                if($state === 0) {
+                    $device->disconnections()->create([
+                        'start_date' => Carbon::now()->toDateTimeString()
+                    ]);
+                }
+            }
+        }
+    }
+}
