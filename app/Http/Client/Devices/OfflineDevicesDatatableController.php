da87d0cbb79e7b5b106ba6e1e5ea25572919aed4
mrebolledo <maxi.rebolledo@gmail.com>
2021-01-18 12:22:05 -0300

    Update OfflineDevicesDatatableController.php

--- app/Http/Client/Devices/OfflineDevicesDatatableController.php
+++ app/Http/Client/Devices/OfflineDevicesDatatableController.php
@@ -26,7 +26,7 @@ class OfflineDevicesDatatableController extends DataTableAbstract
             $record->check_point->name,
             $record->name,
             optional($record->last_dc)->start_date,
-            Carbon::now()->longAbsoluteDiffForHumans(Carbon::parse(optional($record->last_dc)->start_date ?? now()->toDateTimeString()),3),
+            implode(', <br>',explode(' ',Carbon::now()->longAbsoluteDiffForHumans(Carbon::parse(optional($record->last_dc)->start_date ?? now()->toDateTimeString()),3))),
             makeRemoteLink('/client-device-disconnections/'.$record->id,'Log','fa-database','btn-link','btn-sm')
         ];
     }
