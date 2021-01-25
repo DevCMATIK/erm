e2bbc4f54cc1fde1319454c5dd3bbe256609c69c
mrebolledo <maxi.rebolledo@gmail.com>
2021-01-25 20:56:09 -0300

    Update TestController.php

--- app/Http/TestController.php
+++ app/Http/TestController.php
@@ -3,6 +3,8 @@
 namespace App\Http;
 
 use App\App\Controllers\Soap\SoapController;
+use App\Domain\WaterManagement\Device\Sensor\Sensor;
+use App\Domain\WaterManagement\Main\Historical;
 use App\Http\Data\Jobs\Restore\RestoreEnergyConsumption;
 use Carbon\Carbon;
 use Sentinel;
@@ -11,8 +13,40 @@ use Sentinel;
 class TestController extends SoapController
 {
 
+    public $current_date = '2020-12-01';
+
     public function __invoke()
     {
-        RestoreEnergyConsumption::dispatch(1514,'2020-01-02','2021-01-20','2020-01-02')->onQueue('long-running-queue-low');
+        $sensor = Sensor::with(['device','address','type','dispositions','selected_disposition','ranges'])->find($this->sensor_id);
+
+        $rows = Historical::where('grd_id', $sensor->device->internal_id)
+            ->where('register_type',$sensor->address->register_type_id)
+            ->where('address',$sensor->address_number)
+            ->whereRaw("timestamp between '{$this->current_date} 00:00:00' and '{$this->current_date} 23:59:59'")
+            ->get();
+        $current_date = $this->current_date;
+        $newRows = array();
+        for($i= 0;$i<24;$i++) {
+            $hour = str_pad($i, 2, '0', STR_PAD_LEFT);
+            $row = $rows->filter(function($item) use ($hour,$current_date){
+                return (
+                    strtotime($item->timestamp) > strtotime("{$current_date} {$hour}:00:00")
+                    && strtotime($item->timestamp) < strtotime("{$current_date} {$hour}:29:59")
+                );
+            })->first();
+            if($row) {
+                array_push($newRows,$row->toArray());
+            }
+            $row = $rows->filter(function($item) use ($hour,$current_date){
+                return (
+                    strtotime($item->timestamp) > strtotime("{$current_date} {$hour}:30:00")
+                    && strtotime($item->timestamp) < strtotime("{$current_date} {$hour}:59:59")
+                );
+            })->first();
+            if($row) {
+                array_push($newRows,$row->toArray());
+            }
+        }
+        dd($newRows,$rows)
     }
 }
