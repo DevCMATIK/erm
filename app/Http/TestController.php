7fbf7ba6f71c15f4ae157356cfb7f837911fd112
mrebolledo <maxi.rebolledo@gmail.com>
2021-01-26 08:41:32 -0300

    Update TestController.php

--- app/Http/TestController.php
+++ app/Http/TestController.php
@@ -17,36 +17,6 @@ class TestController extends SoapController
 
     public function __invoke()
     {
-        $sensor = Sensor::with(['device','address','type','dispositions','selected_disposition','ranges'])->find(1632);
-
-        $rows = Historical::where('grd_id', $sensor->device->internal_id)
-            ->where('register_type',$sensor->address->register_type_id)
-            ->where('address',$sensor->address_number)
-            ->whereRaw("timestamp between '{$this->current_date} 00:00:00' and '{$this->current_date} 23:59:59'")
-            ->get();
-        $current_date = $this->current_date;
-        $newRows = array();
-        for($i= 0;$i<24;$i++) {
-            $hour = str_pad($i, 2, '0', STR_PAD_LEFT);
-            $row = $rows->filter(function($item) use ($hour,$current_date){
-                return (
-                    strtotime($item->toArray()['timestamp']) > strtotime("{$current_date} {$hour}:00:00")
-                    && strtotime($item->toArray()['timestamp']) < strtotime("{$current_date} {$hour}:29:59")
-                );
-            })->first();
-            if($row) {
-                array_push($newRows,$row->toArray());
-            }
-            $row = $rows->filter(function($item) use ($hour,$current_date){
-                return (
-                    strtotime($item->toArray()['timestamp']) > strtotime("{$current_date} {$hour}:30:00")
-                    && strtotime($item->toArray()['timestamp']) < strtotime("{$current_date} {$hour}:59:59")
-                );
-            })->first();
-            if($row) {
-                array_push($newRows,$row->toArray());
-            }
-        }
-        dd($newRows,$rows);
+        RestoreEnergyConsumption::dispatch(1632,'2020-01-18','2021-01-26')->onQueue('long-running-queue-low');
     }
 }
