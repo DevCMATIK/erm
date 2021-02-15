e1ea79aefc154f686a8a891c3120191a7279d696
mrebolledo <maxi.rebolledo@gmail.com>
2021-02-15 09:57:04 -0300

    Update TestController.php

--- app/Http/TestController.php
+++ app/Http/TestController.php
@@ -17,20 +17,14 @@ class TestController extends SoapController
 
     public function __invoke()
     {
-        $check_points =   CheckPoint::whereNotNull('work_code')
+        $check_points =   CheckPoint::with('last_report')->whereNotNull('work_code')
             ->where('dga_report',1)
             ->get();
 
         foreach($check_points as $check_point) {
-            $reports = CheckPointReport::where('check_point_id',$check_point->id)
-                ->groupBy('date')
-                ->orderBy('date', 'DESC')
-                ->get(array(
-                    DB::raw('Date(report_date) as date'),
-                    DB::raw('COUNT(*) as "reports"')
-                ));
 
-           return $reports->toJson();
+            dd(Carbon::parse($check_point->last_report->report_date)->hour,now()->hour);
+
         }
     }
 }
