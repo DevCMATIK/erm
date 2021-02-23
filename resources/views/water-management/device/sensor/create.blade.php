2f1f3c8f0f5ac641173d07cbe908eb5083576dc6
mrebolledo <maxi.rebolledo@gmail.com>
2021-02-22 21:08:33 -0300

    Update create.blade.php

--- resources/views/water-management/device/sensor/create.blade.php
+++ resources/views/water-management/device/sensor/create.blade.php
@@ -37,7 +37,7 @@
             <input type="text" class="form-control" id="fix_max_value" name="fix_max_value">
         </div>
         <label class="custom-control custom-checkbox">
-            <input type="checkbox"  class="custom-control-input" value="1">
+            <input type="checkbox"  class="custom-control-input" value="1" name="fix_values">
             <span class="custom-control-label">Reparar valores erróneos</span>
         </label>
     </form>
