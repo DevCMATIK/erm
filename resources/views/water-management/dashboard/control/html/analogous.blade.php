6b9a9bdfd7e1c5ffffbe896fd5a877b0b3c8b3bd
mrebolledo <maxi.rebolledo@gmail.com>
2021-02-24 08:54:01 -0300

    Update analogous.blade.php

--- resources/views/water-management/dashboard/control/html/analogous.blade.php
+++ resources/views/water-management/dashboard/control/html/analogous.blade.php
@@ -1,29 +1,12 @@
 @if($analogous_sensor->no_chart_needed != 1)
     <a href="/getDataAsChart/{{ $analogous_sensor->sensor->device_id }}/{{ $analogous_sensor->sensor->id }}"
-       @if($analogous_sensor->sensor->pre_text != '')
-       data-toggle="popover"
-       data-trigger="hover"
-       data-placement="top"
-       title=""
-       data-content="{{ $analogous_sensor->sensor->max_value }} {{ optional(optional($disposition)->unit)->name }}  {{ $analogous_sensor->sensor->post_text ?? '' }}"
-       data-template='<div class="popover bg-primary-500 border-primary" role="tooltip"><div class="arrow"></div><h3 class="popover-header bg-transparent"></h3><div class="popover-body text-white text-center"></div></div>'
-       data-original-title="{{ $analogous_sensor->sensor->pre_text ?? '' }}"
-       @endif
        {!! makeLinkRemote() !!}
        class="d-block  rounded-plus mb-1 px-2 py-1 text-center has-popover"
        id="d_{{$analogous_sensor->id}}_{{$analogous_sensor->sensor->id}}">
 @else
     <div
         class="d-block  rounded-plus mb-1 px-2 py-1 text-center has-popover"
-        @if($analogous_sensor->sensor->pre_text != '')
-        data-toggle="popover"
-        data-trigger="hover"
-        data-placement="top"
-        title=""
-        data-content="{{ $analogous_sensor->sensor->max_value }} {{ optional(optional($disposition)->unit)->name }}  {{ $analogous_sensor->sensor->post_text ?? '' }}"
-        data-template='<div class="popover bg-primary-500 border-primary" role="tooltip"><div class="arrow"></div><h3 class="popover-header bg-transparent"></h3><div class="popover-body text-white text-center"></div></div>'
-        data-original-title="{{ $analogous_sensor->sensor->pre_text ?? '' }}"
-        @endif
+
         id="d_{{$analogous_sensor->id}}_{{$analogous_sensor->sensor->id}}">
 @endif
         <h2 class=" mb-0  @if($off) text-muted @else text-{{ $color }} @endif">
@@ -36,7 +19,7 @@
         </h2>
         <span  class="font-weight-bold text-dark  fs-nano">{{ $analogous_sensor->sensor->name}}</span>
         @if($analogous_sensor->sensor->pre_text != '')
-            <span  class="font-weight-bold text-muted hidden-sm-up  fs-nano"> ({{ $analogous_sensor->sensor->pre_text ?? '' }} {{ $analogous_sensor->sensor->max_value }} {{ optional(optional($disposition)->unit)->name }} {{ $analogous_sensor->sensor->post_text ?? '' }})</span>
+            <span  class="font-weight-bold text-muted   fs-nano"> ({{ $analogous_sensor->sensor->pre_text ?? '' }} {{ $analogous_sensor->sensor->max_value }} {{ optional(optional($disposition)->unit)->name }} {{ $analogous_sensor->sensor->post_text ?? '' }})</span>
         @endif
 @if($analogous_sensor->no_chart_needed != 1)
     </a>
