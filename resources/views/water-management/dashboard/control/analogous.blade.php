5884f491424db856edd08f197c1a6ab30789b14e
mrebolledo <maxi.rebolledo@gmail.com>
2021-02-25 10:36:43 -0300

    Update analogous.blade.php

--- resources/views/water-management/dashboard/control/analogous.blade.php
+++ resources/views/water-management/dashboard/control/analogous.blade.php
@@ -64,6 +64,7 @@
                 foreach($ranges as $range) {
                     if((float)$data >= $range->min && (float)$data <= $range->max) {
                         $color = $range->color;
+                        echo $color;
                     }
                 }
             }
