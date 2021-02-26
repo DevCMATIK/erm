f5b4a1ad619740888779513b13145aaf400ca862
mrebolledo <maxi.rebolledo@gmail.com>
2021-02-26 16:15:33 -0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -62,7 +62,7 @@ return [
             'password' => 'DevAppint'
         ],'erm' => [
             'driver' => 'mysql',
-            'host' => '10.250.128.6',
+            'host' => '34.71.73.66',
             //'host' => '34.66.170.27',
             'port' => '3306',
             'database' => 'watermanagement',
