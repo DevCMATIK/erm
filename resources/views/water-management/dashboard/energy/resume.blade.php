d7219ef129c26caeb8644b17d7d1b4b8cffc7441
mrebolledo <maxi.rebolledo@gmail.com>
2021-04-08 08:43:37 -0400

    mt

--- resources/views/water-management/dashboard/energy/resume.blade.php
+++ resources/views/water-management/dashboard/energy/resume.blade.php
@@ -19,7 +19,7 @@
         }
     </style>
    <div class="row">
-       <div class="col-lg-3 col-xl-3 col-md-6 col-sm-12 ">
+       <div class="col-lg-3 col-xl-3 col-md-6 col-sm-12 mt-sm-2">
            @include('water-management.dashboard.energy.components.main-box', [
                 'bg' => 'bg-primary-300',
                 'value' => $consumptions->reduce(function($carry,$item){
