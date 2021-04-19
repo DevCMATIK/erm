fd1793229b84eb52e39ff83bc30321bc708bffdc
mrebolledo <maxi.rebolledo@gmail.com>
2021-04-19 19:53:44 -0400

    soap header

--- app/App/Jobs/DGA/RestoreToDGA.php
+++ app/App/Jobs/DGA/RestoreToDGA.php
@@ -10,6 +10,7 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use SoapHeader;
 
 class RestoreToDGA extends SoapController implements ShouldQueue
 {
