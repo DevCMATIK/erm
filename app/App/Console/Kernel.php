04adea69915149d0f8642da6f1c126dfe75dc0a8
mrebolledo <maxi.rebolledo@gmail.com>
2021-04-27 16:17:33 -0400

    Revert "disable alarms and emails"
    This reverts commit eef0fab7ab49da0267882b62fef414f283da2262.

--- app/App/Console/Kernel.php
+++ app/App/Console/Kernel.php
@@ -47,10 +47,10 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-       $schedule->job(new ExecuteCommandTriggered(1))->everyMinute();
+        $schedule->job(new ExecuteCommandTriggered(1))->everyMinute();
         $schedule->job(new ExecuteCommandTriggered(5))->everyFiveMinutes();
         $schedule->job(new ExecuteCommandTriggered(10))->everyTenMinutes();
-       /* $schedule->job(new CheckAlarms())->everyMinute();*/
+        $schedule->job(new CheckAlarms())->everyMinute();
 
         $schedule->job(new BackupAnalogousSensors(1),'long-running-queue')->everyMinute();
         $schedule->job(new BackupAnalogousSensors(5),'long-running-queue')->everyFiveMinutes();
@@ -113,14 +113,14 @@ class Kernel extends ConsoleKernel
         $schedule->job(new BackupEnergy('ee-e-aparente'),'long-running-queue-low')->dailyAt('00:40');
         $schedule->job(new BackupWater())->dailyAt('02:20');
         $schedule->job(new CalculateConsumptions(),'long-running-queue-low')->hourlyAt(52);
-       // $schedule->job(new SendReminderMail())->everyThirtyMinutes();
-        //$schedule->job(new NotifyDevicesOffline())->hourly();
+        $schedule->job(new SendReminderMail())->everyThirtyMinutes();
+        $schedule->job(new NotifyDevicesOffline())->hourly();
         $schedule->job(new TrackDisconnectionsFromWater(),'tracking-queue')->everyMinute();
         $schedule->job(new TrackDisconnectionsFromBio(),'tracking-queue')->everyMinute();
         $schedule->job(new TrackDisconnectionsFromDPL(),'tracking-queue')->everyMinute();
 
         // Get all tasks from the database
-       /* $mailReports = MailReport::active()->get();
+        $mailReports = MailReport::active()->get();
         // Go through each task to dynamically set them up.
         foreach ($mailReports as $task) {
             // Use the scheduler to add the task at its desired frequency
@@ -146,7 +146,7 @@ class Kernel extends ConsoleKernel
 
             } while($x-- > 0);
 
-        })->everyMinute();*/
+        })->everyMinute();
     }
 
     /**
