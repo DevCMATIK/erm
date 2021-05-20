1f24c3e3456b9cc42323be2d0048f3b96097e5b4
Dev CMATIK - Felipe <52116993+DevCMATIK@users.noreply.github.com>
2021-05-20 17:59:24 -0400

    Analogus

--- app/App/Console/Kernel.php
+++ app/App/Console/Kernel.php
@@ -52,14 +52,14 @@ class Kernel extends ConsoleKernel
         $schedule->job(new ExecuteCommandTriggered(10))->everyTenMinutes();
         
         $schedule->job(new CheckAlarms())->everyMinute();
-		
+		*/
         $schedule->job(new BackupAnalogousSensors(1),'long-running-queue')->everyMinute();
         $schedule->job(new BackupAnalogousSensors(5),'long-running-queue')->everyFiveMinutes();
         $schedule->job(new BackupAnalogousSensors(10),'long-running-queue')->everyTenMinutes();
         $schedule->job(new BackupAnalogousSensors(15),'long-running-queue')->everyFifteenMinutes();
         $schedule->job(new BackupAnalogousSensors(30),'long-running-queue')->everyThirtyMinutes();
         $schedule->job(new BackupAnalogousSensors(60),'long-running-queue')->hourly();
-
+/*
         $schedule->job(new BackupDigitalSensors(1),'long-running-queue')->everyMinute();
         $schedule->job(new BackupDigitalSensors(5),'long-running-queue')->everyFiveMinutes();
         $schedule->job(new BackupDigitalSensors(10),'long-running-queue')->everyTenMinutes();
