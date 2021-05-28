1cb82f5fb0112a641b6d7367a7a3fc81724d9536
mrebolledo <maxi.rebolledo@gmail.com>
2021-05-27 22:48:15 -0400

    view chart

--- app/Http/WaterManagement/Dashboard/Energy/Controllers/ResumeController.php
+++ app/Http/WaterManagement/Dashboard/Energy/Controllers/ResumeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\WaterManagement\Dashboard\Energy\Controllers;
 
 use App\App\Traits\ERM\HasAnalogousData;
+use App\Domain\Client\Zone\Sub\MapLine;
+use App\Domain\Client\Zone\Sub\SubZone;
 use App\Domain\Client\Zone\Zone;
 use App\Domain\WaterManagement\Device\Sensor\Electric\ElectricityConsumption;
 use App\Domain\WaterManagement\Device\Sensor\Sensor;
@@ -14,42 +16,272 @@ class ResumeController extends Controller
 {
     use HasAnalogousData;
 
-    public function __invoke($zone_id)
+    public function index($zone_id)
     {
         $zone = Zone::with('sub_zones.consumptions')->find($zone_id);
         $consumptions = array();
+        $con = array();
 
         foreach($zone->sub_zones->sortBy('name') as $sub_zone) {
             $monthly = $this->getMonthlyTotal($sub_zone);
             $yesterday = $this->getYesterdayConsumption($sub_zone);
+            if($thisYearTotal = $this->getThisYearTotal($sub_zone)) {
+                $thisYearTotal = $thisYearTotal->toArray();
+            } else {
+                $thisYearTotal = ['consumption' => 0];
+            }
+
+            if($thisMonth = $monthly->where('month',now()->format('Y-m'))->first()) {
+                $thisMonth = $thisMonth->toArray();
+            } else {
+                $thisMonth =['consumption' => 0];
+            }
             array_push($consumptions,[
                 $sub_zone->name => [
-                    'this-year' => $this->getThisYearTotal($sub_zone)->toArray(),
+                    'this-year' => $thisYearTotal,
                     'monthly' => $monthly->toArray(),
-                    'this-month' => $monthly->where('month',now()->format('Y-m'))->first()->toArray(),
-                    'yesterday' => $yesterday->consumption,
-                    'today' => $this->getTodayConsumption($sub_zone,$yesterday->last_read)
+                    'this-month' => $thisMonth,
+                    'yesterday' => $yesterday->consumption ?? 0,
+                    'today' => $this->getTodayConsumption($sub_zone,$yesterday->last_read ?? 0)
                 ]
             ]);
         }
+        foreach($zone->sub_zones->sortBy('name') as $sub_zone) {
+            $monthly = $this->getMonthlyTotalCon($sub_zone);
+            //$yesterday = $this->getYesterdayConsumption($sub_zone);
+            array_push($con,
+                [
+                    $sub_zone->name => $monthly->toArray()
+                ]
+            );
+        }
+        $con = collect($con)->collapse();
+        $months = array_keys(collect($con->first())->collapse()->toArray());
+        $years = collect($months)->map(function($month){
+            return explode('-',$month)[0];
+        })->unique();
+
+        if($zone_id == 11) {
+            $subZones = $this->getSubZones($zone_id);
+            $lines = $this->getLines($subZones);
+        } else {
+            $subZones = [];
+            $lines = [];
+        }
         return view('water-management.dashboard.energy.resume', [
             'zone' => $zone,
-            'consumptions' => collect($consumptions)
+            'consumptions' => collect($consumptions),
+            'months' => $months,
+            'sub_zones' => $zone->sub_zones->map(function($item){
+                return str_replace(' TG-1','',str_replace(' TG-2','',$item->name));
+            })->unique()->toArray(),
+            'subZones' => $subZones,
+            'lines' => $lines,
+            'years' => $years
         ]);
     }
+
+    protected function getLines($subZones)
+    {
+        $subZones = collect($subZones);
+        return MapLine::with(['p_one','p_two'])->orderBy('position')->get()->map(function($item) use ($subZones) {
+            $l = array();
+
+            array_push($l,[
+                'lng' => $item->p_one->lng,
+                'lat' => $item->p_one->lat
+            ]);
+
+            if($item->points_between != null) {
+                foreach(json_decode($item->points_between) as $point) {
+                    $coords = explode(',',str_replace(' ','',$point));
+                    array_push($l,[
+                        'lng' => $coords[1],
+                        'lat' => $coords[0]
+                    ]);
+                }
+            }
+
+            array_push($l,[
+                'lng' => $item->p_two->lng,
+                'lat' => $item->p_two->lat
+            ]);
+
+            if($subZones->where('id',$item->point_one)->first()['status']['state'] == 1){
+                $color = '#6AD252';
+            } else {
+                $color = '#B2BABB';
+            }
+            return [
+                'lines' => $l,
+                'color' => $color
+            ];
+        });
+    }
+
+    protected function getSubZones($id)
+    {
+        return  SubZone::with('check_points.devices.report')
+            ->where('zone_id',$id)
+            ->get()
+            ->map(function($subZone){
+                return [
+                    'id' => $subZone->id,
+                    'name' => $subZone->name,
+                    'lat' => $subZone->lat,
+                    'lng' => $subZone->lng,
+                    'status' => $subZone->check_points->map(function($checkPoint){
+                        $okCount = 0;
+                        $offCount = 0;
+                        foreach($checkPoint->devices->map(function($device){
+                            return $device->report->state;
+                        }) as $state){
+                            if($state == 0) {
+                                $offCount++;
+                            } else {
+                                $okCount++;
+                            }
+                        }
+
+                        if($offCount > 0 && $okCount <= 0) {
+                            return ['state' => 0, 'color' => '#F74C41' ]; //rojo
+                        } elseif ($offCount > 0 && $okCount > 0) {
+                            return ['state' => 0, 'color' => '#ACACAC' ]; //plomo
+                        } else {
+                            return ['state' => 1, 'color' => '#6AD252' ]; //verde
+                        }
+                    })->toArray()[0]
+                ];
+            });
+    }
+
+    protected function getMonthlyTotalCon($sub_zone)
+    {
+        return ElectricityConsumption::select(
+            DB::raw('sum(consumption) as consumption'),
+            DB::raw("DATE_FORMAT(date,'%Y-%m') as month")
+        )->where('sensor_type','ee-e-activa')
+            ->where('sub_zone_id',$sub_zone->id)
+            ->groupBy('month')
+            ->get()->map(function($item){
+                return [
+                    $item['month'] => $item['consumption']
+                ];
+            });
+    }
+
+    public function chartData(Request $request,$zone_id)
+    {
+        $data['series'] = array();
+        array_push($data['series'] , [
+            'name' => "Consumo Energía Mensual",
+            'data' => $this->makeSeries($request,$zone_id),
+        ]) ;
+
+        return json_encode($data,JSON_NUMERIC_CHECK);
+    }
+
+    protected function makeSeries(Request $request,$zone_id)
+    {
+        $zone = Zone::with('sub_zones.consumptions')->find($zone_id);
+        $consumptions = array();
+        foreach($zone->sub_zones->sortBy('name') as $sub_zone) {
+            $monthly = $this->getMonthlyTotalCon($sub_zone);
+            //$yesterday = $this->getYesterdayConsumption($sub_zone);
+            array_push($consumptions,
+                [
+                    $sub_zone->name => $monthly->toArray()
+                ]
+            );
+        }
+        $consumptions = collect($consumptions)->collapse();
+        $rows = array();
+        if($request->sub_zone != '') {
+            foreach ($consumptions as $sub_zone => $consumption) {
+                $name = str_replace(' TG-1','',str_replace(' TG-2','',$sub_zone));
+                foreach(collect($consumption)->collapse() as $key => $data) {
+                    if($row = collect($rows)->where('sub_zone',$name)->where('month',$key)->first()) {
+                        $rows =  collect($rows)->map(function($item) use($name,$data,$key){
+                            if($item['sub_zone'] == $name && $item['month'] == $key) {
+                                return [
+                                    'sub_zone' => $name,
+                                    'month' => $key,
+                                    'consumption' => $item['consumption'] +$data,
+                                ];
+                            } else {
+                                return $item;
+                            }
+                        })->toArray();
+                    } else {
+                        array_push($rows,[
+                            'sub_zone' => $name,
+                            'month' => $key,
+                            'consumption' => $data
+                        ]);
+                    }
+
+                }
+            }
+            $rows = collect($rows)->where('sub_zone',$request->sub_zone);
+        } else {
+            foreach ($consumptions as $sub_zone => $consumption) {
+                foreach (collect($consumption)->collapse() as $key => $data) {
+                    if ($row = collect($rows)->where('month', $key)->first()) {
+                        $rows = collect($rows)->map(function ($item) use ($data, $key) {
+                            if ($item['month'] == $key) {
+                                return [
+                                    'month' => $key,
+                                    'consumption' => $item['consumption'] + $data,
+                                ];
+                            } else {
+                                return $item;
+                            }
+                        })->toArray();
+                    } else {
+                        array_push($rows, [
+                            'month' => $key,
+                            'consumption' => $data
+                        ]);
+                    }
+
+                }
+            }
+        }
+        $rows = collect($rows)->whereIn('month',$request->months);
+
+        $array = array();
+        foreach ($rows as $key => $row) {
+            array_push($array, [
+                'x' => (strtotime($row['month'].'-01'))*1000,
+                'y' => (int)$row['consumption'],
+                'name' => $row['month']
+            ]);
+
+        }
+        return $array;
+    }
+
 
     protected function getTodayConsumption($sub_zone,$last_read)
     {
         $sensor = Sensor::
         with(['device.report','dispositions.unit'])
             ->find($sub_zone->consumptions
-                ->where('sensor_type')
-                ->first()
-                ->sensor_id
+                    ->where('sensor_type')
+                    ->first()
+                    ->sensor_id ?? null
             );
-        $value = $this->getAnalogousValue($sensor);
+        if($sensor) {
+            $value = $this->getAnalogousValue($sensor);
+            if(is_array($value )) {
+                $val = $value['value'] ;
+            } else {
+                $val = 0;
+            }
+            return number_format( ((($last_read == 0)?0:$val) - $last_read),2);
+        }
 
-        return number_format( ($value['value'] - $last_read),2);
+        return 0;
     }
 
     protected function getYesterdayConsumption($sub_zone)
--- resources/views/water-management/dashboard/energy/resume.blade.php
+++ resources/views/water-management/dashboard/energy/resume.blade.php
@@ -18,54 +18,115 @@
             }
         }
     </style>
-   <div class="row">
-       <div class="col-lg-3 col-xl-3 col-md-6 col-sm-12">
-           @include('water-management.dashboard.energy.components.main-box', [
-                'bg' => 'bg-primary-300',
-                'value' => $consumptions->reduce(function($carry,$item){
-                                return $carry + (is_numeric($item[key($item)]['today'])?$item[key($item)]['today']:0);
-                           }),
-                'unit' => 'kWh',
-                'title' => 'Consumo Hoy',
-                'icon' => 'fa-bolt'
-            ])
-       </div>
-       <div class="col-lg-3 col-xl-3 col-md-6 col-sm-12">
-           @include('water-management.dashboard.energy.components.main-box', [
-                'bg' => 'bg-primary-300',
-                'value' => $consumptions->reduce(function($carry,$item){
-                                return $carry + $item[key($item)]['yesterday'];
-                           }),
-                'unit' => 'kWh',
-                'title' => 'Consumo Ayer',
-                'icon' => 'fa-bolt'
-            ])
-       </div>
+    <div class="row">
+        <div class="col-lg-3 col-xl-3 col-md-6 col-sm-12 mt-sm-2">
+            @include('water-management.dashboard.energy.components.main-box', [
+                 'bg' => 'bg-primary-300',
+                 'value' => $consumptions->reduce(function($carry,$item){
+                                 return $carry + (is_numeric($item[key($item)]['today'])?$item[key($item)]['today']:0);
+                            }),
+                 'unit' => 'kWh',
+                 'title' => 'Consumo Hoy',
+                 'icon' => 'fa-bolt'
+             ])
+        </div>
+        <div class="col-lg-3 col-xl-3 col-md-6 col-sm-12 mt-sm-2">
+            @include('water-management.dashboard.energy.components.main-box', [
+                 'bg' => 'bg-primary-300',
+                 'value' => ($consumptions->reduce(function($carry,$item){
+                                 return $carry + $item[key($item)]['yesterday'];
+                            }) <= 0)? 'No calculado aún' : $consumptions->reduce(function($carry,$item){
+                                 return $carry + $item[key($item)]['yesterday'];
+                            }),
+                 'unit' => 'kWh',
+                 'title' => 'Consumo Ayer',
+                 'icon' => 'fa-bolt'
+             ])
+        </div>
 
-       <div class="col-lg-3 col-xl-3 col-md-6 col-sm-12">
-           @include('water-management.dashboard.energy.components.main-box', [
-                'bg' => 'bg-primary',
-                'value' => $consumptions->reduce(function($carry,$item){
-                                return $carry + $item[key($item)]['this-month']['consumption'];
-                           }),
-                'unit' => 'kWh',
-                'title' => 'Consumo este mes',
-                'icon' => 'fa-calendar'
-            ])
-       </div>
-       <div class="col-lg-3 col-xl-3 col-md-6 col-sm-12">
-           @include('water-management.dashboard.energy.components.main-box', [
-                'bg' => 'bg-primary',
-                'value' => $consumptions->reduce(function($carry,$item){
-                                return $carry + $item[key($item)]['this-year']['consumption'];
-                           }),
-                'unit' => 'kWh',
-                'title' => 'Consumo este año',
-                'icon' => 'fa-calendar'
-            ])
-       </div>
-   </div>
+        <div class="col-lg-3 col-xl-3 col-md-6 col-sm-12 mt-sm-2">
+            @include('water-management.dashboard.energy.components.main-box', [
+                 'bg' => 'bg-primary',
+                 'value' => $consumptions->reduce(function($carry,$item){
+                                 return $carry + $item[key($item)]['this-month']['consumption'];
+                            }),
+                 'unit' => 'kWh',
+                 'title' => 'Consumo este mes',
+                 'icon' => 'fa-calendar'
+             ])
+        </div>
+        <div class="col-lg-3 col-xl-3 col-md-6 col-sm-12 mt-sm-2">
+            @include('water-management.dashboard.energy.components.main-box', [
+                 'bg' => 'bg-primary',
+                 'value' => $consumptions->reduce(function($carry,$item){
+                                 return $carry + ($item[key($item)]['this-year']['consumption'] ?? 0);
+                            }),
+                 'unit' => 'kWh',
+                 'title' => 'Consumo este año',
+                 'icon' => 'fa-calendar'
+             ])
+        </div>
+    </div>
+    @if(count($lines) > 0)
+        <div class="row px-2 mb-2 my-4">
+            <div class="col" id="map" style="min-height: 600px;">
 
+            </div>
+        </div>
+    @endif
+    <div class="row mt-4">
+        <div class="col">
+            <div class="card">
+                <div class="card-body">
+                    <ul class="nav nav-pills justify-content-end" role="tablist">
+                        <li class="nav-item">
+                            <div class="form-group mr-3">
+                                <select name="sub_zone_id" id="sub_zone_id" class="form-control">
+                                    <option value="">Todas las Sub zonas</option>
+                                    @foreach($sub_zones as $sub_zone)
+                                        <option value="{{ $sub_zone }}">{{ $sub_zone }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </li>
+                        <li class="nav-item" id="sensor_list_dropdown">
+                            <a class="form-control border " href="javascript:void(0);" data-toggle="dropdown">Meses <i class="fal fa-chevron-down"></i></a>
+                            <ul class="dropdown-menu " style="max-height: 300px; overflow-y: auto;">
+                                @foreach(array_reverse($years->toArray()) as $year)
+                                    <li class="list-group-item">
+                                        <label class="custom-control custom-checkbox mb-2">
+                                            <input type="checkbox" id="check_{{$year}}" class="custom-control-input sensors year-check" value="{{ $year }}" checked name="years">
+                                            <span class="custom-control-label">{{ $year }}</span>
+                                        </label>
+                                        @php
+                                            \Carbon\Carbon::setLocale('es');
+                                            setlocale(LC_ALL, 'es_ES');
+                                        @endphp
+                                        @foreach($months as $month)
+
+                                            @if(stristr($month,$year))
+                                                <label class="custom-control custom-checkbox ml-4">
+                                                    <input type="checkbox" class="custom-control-input sensors check_{{ $year }}" value="{{ $month }}" checked name="months">
+                                                    <span class="custom-control-label">{{ \Carbon\Carbon::parse($month.'-01')->formatLocalized('%B')  }}</span>
+                                                </label>
+                                            @endif
+                                        @endforeach
+                                    </li>
+                                @endforeach
+
+                            </ul>
+                        </li>
+
+
+                    </ul>
+
+                    <div class="row">
+                        <div class="col" id="chartContainer"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <div class="row mt-4">
         <div class="col-12">
             <table class="table m-0 table-bordered table-light table-striped table-dark">
@@ -80,16 +141,16 @@
                 </tr>
                 </thead>
                 <tbody>
-                    @foreach($consumptions as $consumption)
-                        <tr>
-                            <td>{{ key($consumption) }}</td>
-                            <td><strong>{{ (is_numeric($consumption[key($consumption)]['today']))?number_format($consumption[key($consumption)]['today'],1,',','.'):'0'}}</strong> <span class="fs-nano">kWh</span></td>
-                            <td><strong>{{ (is_numeric($consumption[key($consumption)]['yesterday']))?number_format($consumption[key($consumption)]['yesterday'],1,',','.'):'0'}}</strong> <span class="fs-nano">kWh</span></td>
-                            <td><strong>{{ (is_numeric($consumption[key($consumption)]['this-month']['consumption']))?number_format($consumption[key($consumption)]['this-month']['consumption'],1,',','.'):'0'}}</strong> <span class="fs-nano">kWh</span></td>
-                            <td><strong>{{ (is_numeric($consumption[key($consumption)]['this-year']['consumption']))?number_format($consumption[key($consumption)]['this-year']['consumption'],1,',','.'):'0'}}</strong> <span class="fs-nano">kWh</span></td>
+                @foreach($consumptions as $consumption)
+                    <tr>
+                        <td>{{ key($consumption) }}</td>
+                        <td><strong>{{ (is_numeric($consumption[key($consumption)]['today']))?number_format($consumption[key($consumption)]['today'],1,',','.'):'0'}}</strong> <span class="fs-nano">kWh</span></td>
+                        <td><strong>{{ (is_numeric($consumption[key($consumption)]['yesterday']))?number_format($consumption[key($consumption)]['yesterday'],1,',','.'):'0'}}</strong> <span class="fs-nano">kWh</span></td>
+                        <td><strong>{{ (is_numeric($consumption[key($consumption)]['this-month']['consumption']))?number_format($consumption[key($consumption)]['this-month']['consumption'],1,',','.'):'0'}}</strong> <span class="fs-nano">kWh</span></td>
+                        <td><strong>{{ (is_numeric($consumption[key($consumption)]['this-year']['consumption']))?number_format($consumption[key($consumption)]['this-year']['consumption'],1,',','.'):'0'}}</strong> <span class="fs-nano">kWh</span></td>
 
-                        </tr>
-                    @endforeach
+                    </tr>
+                @endforeach
                 </tbody>
             </table>
         </div>
@@ -97,12 +158,141 @@
 @endsection
 @section('more-scripts')
     {!! includeScript([
-		   'plugins/highcharts/highcharts.js',
-		   'plugins/highcharts/modules/exporting.js',
-	   ]) !!}
+         'plugins/highcharts/highcharts.js',
+         'plugins/highcharts/modules/exporting.js',
+     ]) !!}
+    @include('water-management.dashboard.energy.chart.chart', ['zone_id' => $zone->id])
+
+    <script>
+        function getFilters()
+        {
+            return {
+                months : $.map($('input[name="months"]:checked'), function(c){return c.value; }),
+                sub_zone : $('#sub_zone_id').val()
+            };
+        }
+        $('.year-check').on('click',function() {
+            let year = $(this).val();
+            $('.check_'+year).prop('checked',$(this).is(':checked'));
+            renderChart()
+        });
+
+        $('#sub_zone_id').on('change',function() {
+            renderChart();
+        })
+
+        $('input[name="months"]').on('click',function() {
+            let year = $(this).val().split('-')[0];
+            let checks = document.querySelectorAll('.check_'+year)
+            let allChecked = true;
+
+            for (let i = 0; i < checks.length; i++) {
+                if(!checks[i].checked) {
+                    allChecked = false;
+                }
+            }
+            $('#check_'+year).prop('checked',allChecked)
+            renderChart();
+
+        });
+        renderChart();
+    </script>
 @endsection
 @section('page-extra-scripts')
     {!! includeScript('plugins/bootstrap-daterangepicker/daterangepicker.js') !!}
 
 @endsection
+
+@section('maps-content')
+    @if(count($lines) > 0)
+        <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+
+        <script
+            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyANLIUC0DU8-V7nWP7CvP76Bp67MQDxTZY&callback=initMap&libraries=visualization&v=weekly"
+            defer
+        ></script>
+        <script src="https://unpkg.com/@googlemaps/markerclustererplus/dist/index.min.js"></script>
+
+        <script>
+
+            let map;
+            function initMap() {
+
+                map = new google.maps.Map(document.getElementById("map"), {
+                    zoom: 14,
+                    center: { lat: -33.973347542860296, lng: -71.65837822592083},
+                    mapTypeId: 'hybrid',
+                });
+
+                const image =
+                    "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png";
+
+                var markers = [];
+                var data = {!! json_encode($subZones->toArray(),JSON_NUMERIC_CHECK) !!};
+
+                data.forEach(function(item, i) {
+                    var infoWindow = new google.maps.InfoWindow({});
+                    var marker = new google.maps.Marker({
+                        map: map,
+                        position: new google.maps.LatLng(item.lat,item.lng),
+                        icon:  pinSymbol(item.status.color)
+                    });
+                    markers.push(new google.maps.LatLng(item.lat,item.lng));
+
+                    google.maps.event.addListener(marker, 'mouseover', function () {
+                        infoWindow.setContent('<div><strong>' + item.name + '</strong><br>');
+                        infoWindow.open(map, marker); // open at marker's location
+                    });
+
+                    google.maps.event.addListener(marker, 'mouseout', function () {
+                        infoWindow.close();
+                    });
+                })
+
+                var lines = {!! json_encode($lines,JSON_NUMERIC_CHECK) !!};
+
+                lines.forEach(function(item, i) {
+                    var path = [];
+                    item.lines.forEach(function(point, i) {
+                        path.push(new google.maps.LatLng(point.lat,point.lng));
+                    });
+                    var linePath = new google.maps.Polyline({
+                        path: path,
+                        geodesic: true,
+                        strokeColor: item.color,
+                        strokeWeight : 6,
+                        map : map
+                    });
+                })
+
+
+
+                //var markerCluster = new MarkerClusterer(map, markers);
+
+
+            }
+
+            function pinSymbol(color) {
+                return {
+                    path: 'M 0,0 C -2,-20 -10,-22 -10,-30 A 10,10 0 1,1 10,-30 C 10,-22 2,-20 0,0 z M -2,-30 a 2,2 0 1,1 4,0 2,2 0 1,1 -4,0',
+                    fillColor: color,
+                    fillOpacity: 1,
+                    strokeColor: '#000',
+                    strokeWeight: 2,
+                    scale: 1,
+                };
+            }
+
+
+            function getZonesData()
+            {
+                var markers = [];
+
+                return markers;
+            }
+
+        </script>
+    @endif
+@endsection
+
 
