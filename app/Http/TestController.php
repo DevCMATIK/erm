3aa8fe0ce3d773df9e63ba8a4e7b93a5938eaf7f
mrebolledo <maxi.rebolledo@gmail.com>
2021-05-28 01:19:36 -0400

    Update TestController.php

--- app/Http/TestController.php
+++ app/Http/TestController.php
@@ -28,27 +28,47 @@ class TestController extends SoapController
 
     public function __invoke(Request $request)
     {
-        $checkpoint = CheckPoint::find(206);
+        $toInsert = array();
+        $first_date = Carbon::yesterday()->toDateString();
+        $second_date = Carbon::today()->toDateString();
+        $sensors =  Sensor::whereHas('type', $typeFilter = function ($q) {
+            return $q->where('slug',$this->sensor_type);
+        })->whereHas('analogous_reports', $reportsFilter = function($query) use ($first_date,$second_date){
+            return $query->whereRaw("date between '{$first_date} 00:00:00' and '{$second_date} 00:01:00'");
+        })->with([
+            'type' => $typeFilter,
+            'device.check_point.sub_zones',
+            'analogous_reports' => $reportsFilter,
+            'consumptions'
+        ])->get();
 
-        $sensors = $this->getSensors($checkpoint->id);
+        foreach($sensors as $sensor) {
+            if(count($sensor->consumptions) > 0) {
+                $first_read = $sensor->consumptions->sortByDesc('date')->first()->last_read;
+                $last_read = $sensor->analogous_reports->sortByDesc('date')->first()->result;
+            } else {
+                $first_read = $sensor->analogous_reports->sortBy('date')->first()->result;
+                $last_read = $sensor->analogous_reports->sortByDesc('date')->first()->result;
+            }
+            $consumption_yesterday = $sensor->consumptions->where('date',Carbon::yesterday()->toDateString())->first();
 
-        $analogous_reports = AnalogousReport::whereIn('sensor_id',$sensors->pluck('id')->toArray())
-            ->whereRaw("date between '2021-05-08 12:00:00' and '2021-05-08 12:59:00'")->get();
+                if($first_read && $last_read) {
+                    $consumption = $last_read - $first_read;
 
-        $tote =  $analogous_reports->where('sensor_id',$this->getToteSensor($sensors)->id)
-                ->where("date", '>=','2021-05-08 12:00:00')->where('date','<=','2021-05-08 12:59:00')
-                ->first()->result ?? 0;
+                    array_push($toInsert,[
+                        'sensor_id' => $sensor->id,
+                        'first_read' => $first_read,
+                        'last_read' => $last_read,
+                        'consumption' => $consumption,
+                        'sensor_type' => $sensor->type->slug,
+                        'sub_zone_id' => $sensor->device->check_point->sub_zones->first()->id,
+                        'date' => Carbon::yesterday()->toDateString()
+                    ]);
+                }
 
-        $flow =  $analogous_reports->where('sensor_id',$this->getFlowSensor($sensors)->id)
-                ->where("date", '>=','2021-05-08 12:00:00')->where('date','<=','2021-05-08 12:59:00')
-                ->first()->result ?? 0;
+        }
 
-        $level =  $analogous_reports->where('sensor_id',$this->getLevelSensor($sensors)->id)
-                ->where("date", '>=','2021-05-08 12:00:00')->where('date','<=','2021-05-08 12:59:00')
-                ->first()->result ?? 0;
-
-        RestoreToDGA::dispatch($tote,$flow,$level,$checkpoint->work_code,$checkpoint,'2021-05-08 12:00:00')->onQueue('long-running-queue-low');
-        dd($tote,$flow,$level);
+        return $this->testResponse([$toInsert]);
     }
 
     protected function getLines($subZones)
