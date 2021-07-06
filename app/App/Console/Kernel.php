98bd2b2c33eee1014a413fcceeddeff098080666
mrebolledo <maxi.rebolledo@gmail.com>
2021-07-06 11:10:30 -0400

    apikey

--- app/App/Console/Kernel.php
+++ app/App/Console/Kernel.php
@@ -47,6 +47,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        //test
         /*$schedule->job(new ExecuteCommandTriggered(1))->everyMinute();
         $schedule->job(new ExecuteCommandTriggered(5))->everyFiveMinutes();
         $schedule->job(new ExecuteCommandTriggered(10))->everyTenMinutes();
---
+++ app/Domain/Api/ApiKey.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Domain\Api;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ApiKey extends Model
+{
+    //
+}
